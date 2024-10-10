713cf45d41d90b4290edfe13ddd91b5a414ccf9d
Jonathan Matthew Brunt <jon.m.brunt@gmail.com>
2024-10-10 12:22:35 -0300

    MINOR UPDATES

--- app/chain.py
+++ app/chain.py
@@ -3,7 +3,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import os
 
-llm = ChatOpenAI(model="gpt-3.5-turbo")
+llm = ChatOpenAI(model="gpt-4o-mini")
 
 _TEMPLATE = "Answer the following question as best you can, and end the question with a follow up question: {question}"
 
